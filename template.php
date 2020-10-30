e9161a56f9ba2f9a82ec9d1b6ee966414ba78b89
Veeranjay Singh <veeranjaysingh2601@gmail.com>
2020-10-30 13:14:27 +0530

    change

--- template.php
+++ template.php
@@ -1,29 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Login</title>
+	<title>Welcome</title>
 	<link rel="stylesheet" href="Resources\style.css">
 	<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
 	<link rel="icon" href="Resources\home-page.png">
 	<style>
-    body{
-      background-color:  #1c1c1c;
-    }
 		body::-webkit-scrollbar {
 			font-family: Open Sans;
 			display: none;
 			-ms-overflow-style: none;
   		scrollbar-width: none;
 		}
-    .Login{
-      margin: auto;
-      margin-top: 100px;
-      width: 600px;
-      height: 600px;
-      background-color: white;
-      border-radius: 5%;
-
-    }
+		.background-image{
+			width: 100%;
+			height: 500px;
+			padding-bottom: 700px;
+			background-image: url(Resources/Banner.png);
+			background-repeat: no-repeat;
+			background-size: cover;
+			background-position: center;
+			background-attachment: fixed;
+		}
+		.mid-section{
+			margin-top: 10px;
+			background-color: #1c1c1c;
+			width: 100%;
+			height: 710px;
+		}
 	</style>
 </head>
 <body>
@@ -33,7 +37,7 @@
 			<img class="navbar-brand" src="Resources/icon-white.png" alt="image"></img>
 		</div>
 		<ul class="nav navbar-nav">
-			<li class="active"><a href="index.html">Home</a></li>
+			<li class="active"><a href="#">Home</a></li>
 			<li><a href="#">Portfolio</a></li>
 			<li><a href="#">Projects</a></li>
 		</ul>
@@ -42,7 +46,9 @@
 	    </ul>
 	  </div>
 	</nav>
-  <div class="Login"></div>
 
+<div class="background-image"></div>
+
+<div class="mid-section"></div>
 </body>
 </html>
